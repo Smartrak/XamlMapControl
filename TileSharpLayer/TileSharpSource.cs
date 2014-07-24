d0b0c2edcedb75aea2e40c3f9fc41feb20959dd5
danzel <danzel@localhost.geek.nz>
2014-07-24 14:42:22 +1200

    Add a layer type that can get TileSharp to generate tiles without needing to use http

---
+++ TileSharpLayer/TileSharpSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using MapControl;
+using TileSharp;
+
+namespace TileSharpLayer
+{
+	public class TileSharpSource : ImageTileSource
+	{
+		private readonly ILabelOverlapPreventer _overlapPreventer;
+		private readonly IFeatureCache _featureCache;
+		private readonly LayerConfig _layerConfig;
+
+		public TileSharpSource(ILabelOverlapPreventer overlapPreventer, IFeatureCache featureCache, LayerConfig layerConfig)
+		{
+			IsAsync = true;
+
+			_overlapPreventer = overlapPreventer;
+			_featureCache = featureCache;
+			_layerConfig = layerConfig;
+		}
+
+		public override ImageSource LoadImage(int x, int y, int zoomLevel, int rotation)
+		{
+			using (var res = new TileSharp.GdiRenderer.Renderer(_overlapPreventer, _featureCache).GenerateTile(new TileConfig(zoomLevel, x, y, _layerConfig, rotation)))
+			{
+				var image = ImageFromBitmap(res);
+				image.Freeze();
+				return image;
+			}
+		}
+
+		private ImageSource ImageFromBitmap(Bitmap bmp)
+		{
+			IntPtr hBitmap = bmp.GetHbitmap();
+
+			try
+			{
+				return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+			}
+			finally
+			{
+				DeleteObject(hBitmap);
+			}
+		}
+
+		[System.Runtime.InteropServices.DllImport("gdi32.dll")]
+		public static extern bool DeleteObject(IntPtr hObject);
+	}
+}
---
+++ TileSharpLayer/TileSharpTileLayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using MapControl;
+using TileSharp;
+using TileSharp.LabelOverlapPreventers;
+
+namespace TileSharpLayer
+{
+	public class TileSharpTileLayer : TileLayer
+	{
+		public TileSharpTileLayer()
+		{
+			OverlapPreventer = new PerfectQuadtreePreventer();
+			FeatureCache = new NullCache();
+		}
+
+		#region LayerConfig
+		public LayerConfig LayerConfig
+		{
+			get { return (LayerConfig)GetValue(LayerConfigProperty); }
+			set { SetValue(LayerConfigProperty, value); }
+		}
+
+		private static readonly Type LayerConfigPropertyType = typeof(LayerConfig);
+
+		public static readonly DependencyProperty LayerConfigProperty = DependencyProperty.Register(
+			"LayerConfig", LayerConfigPropertyType, typeof(TileSharpTileLayer),
+			new PropertyMetadata(null, (o, e) => ((TileSharpTileLayer)o).ConfigChanged()));
+		#endregion
+
+		#region OverlapPreventer
+		public ILabelOverlapPreventer OverlapPreventer
+		{
+			get { return (ILabelOverlapPreventer)GetValue(OverlapPreventerProperty); }
+			set { SetValue(OverlapPreventerProperty, value); }
+		}
+
+		private static readonly Type OverlapPreventerPropertyType = typeof(ILabelOverlapPreventer);
+
+		public static readonly DependencyProperty OverlapPreventerProperty = DependencyProperty.Register(
+			"OverlapPreventer", OverlapPreventerPropertyType, typeof(TileSharpTileLayer),
+			new PropertyMetadata(null, (o, e) => ((TileSharpTileLayer)o).ConfigChanged()));
+		#endregion
+
+		#region FeatureCache
+		public IFeatureCache FeatureCache
+		{
+			get { return (IFeatureCache)GetValue(FeatureCacheProperty); }
+			set { SetValue(FeatureCacheProperty, value); }
+		}
+
+		private static readonly Type FeatureCachePropertyType = typeof(IFeatureCache);
+
+		public static readonly DependencyProperty FeatureCacheProperty = DependencyProperty.Register(
+			"FeatureCache", FeatureCachePropertyType, typeof(TileSharpTileLayer),
+			new PropertyMetadata(null, (o, e) => ((TileSharpTileLayer)o).ConfigChanged()));
+		#endregion
+
+
+		private void ConfigChanged()
+		{
+			TileSource = new TileSharpSource(OverlapPreventer, FeatureCache, LayerConfig);
+		}
+	}
+}
